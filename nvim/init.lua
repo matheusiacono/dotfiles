0e44416e6003ac9acefd203013ee71eca97554f3
Matheus Oliveira <m.iacono@jucr.de>
2023-01-13 17:25:33 +0100

    chore(nvim): move global to config file

--- nvim/init.lua
+++ nvim/init.lua
@@ -348,7 +348,6 @@ local servers = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
-      diagnostics = { globals = { 'vim' } },
     },
   },
 }
